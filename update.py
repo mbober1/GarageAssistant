79b7dcfc9954d6fb33898513b31ef91f1f694358
Marcin Bober <mbober1@gmail.com>
2021-02-15 14:15:43 +0100

    updater fic

--- update.py
+++ update.py
@@ -8,3 +8,6 @@ dir += str(projenv["UNIX_TIME"])
 dir += ".bin"
 shutil.copyfile(".pio/build/nodemcuv2/firmware.bin", dir)
 print(dir)
+
+with open('version', 'w') as f:
+    f.write(str(projenv["UNIX_TIME"]))
