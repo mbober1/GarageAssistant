a0c99e31908424ac73a71fd8f27636ea1ca38f82
Marcin Bober <mbober1@gmail.com>
2021-02-15 14:02:02 +0100

    updater

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <MQTT.h>
 #include <ESP8266WiFi.h>
+// #include <ESPhttpUpdate.h>
 #include "secrets.hpp"
 
 
@@ -14,18 +15,19 @@ char buffer2[100];
 
 
 void connect() {
-  Serial.print("Connecting to ");
-  Serial.print(ssid);
+  Serial.printf("Connecting to %s ", ssid);
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
-    delay(1000);
+    delay(500);
   }
   Serial.println("\nConnected to WiFi!");
 
-  Serial.print("\nConnecting to Home Assistant");
+  Serial.printf("\nConnecting to %s", mqtt_server);
+  mqtt.begin(mqtt_server, wifi);
   while (!mqtt.connect(project_name, mqtt_user, mqtt_pass)) {
     Serial.print(".");
-    delay(1000);
+    delay(500);
   }
   Serial.println("\nConnected to Home Assistant!");
 }
@@ -34,17 +36,38 @@ void configure() {
   sprintf(buffer, "homeassistant/binary_sensor/%s/car/config", mqtt_dir);
   sprintf(buffer2, "{\"name\": \"%s\", \"state_topic\": \"homeassistant/binary_sensor/%s/car/state\"}", entity_name, mqtt_dir);
   mqtt.publish(buffer, buffer2);
-  Serial.println("Configured!");
+  Serial.println("Configured MQTT!");
 }
 
 void setup() {
   Serial.begin(115200);
+  delay(1000);
+  Serial.printf("Version: %d\n", LAST_BUILD_TIME);
   pinMode(LED_BUILTIN, OUTPUT);
-  WiFi.begin(ssid, password);
-  mqtt.begin(mqtt_server, wifi);
+
 
   connect();
   configure();
+
+
+  // t_httpUpdate_return ret = ESPhttpUpdate.update("192.168.0.2", 80, "/esp/update/arduino.php", LAST_BUILD_TIME);
+  // switch(ret) {
+  //   case HTTP_UPDATE_FAILED:
+  //       Serial.println("[update] Update failed.");
+  //       break;
+  //   case HTTP_UPDATE_NO_UPDATES:
+  //       Serial.println("[update] Update no Update.");
+  //       break;
+  //   case HTTP_UPDATE_OK:
+  //       Serial.println("[update] Update ok."); // may not be called since we reboot the ESP
+  //       break;
+  // }
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+
 }
 
 void loop() {
---
+++ update.py
@@ -0,0 +1,10 @@
+import shutil, os
+Import("env", "projenv")
+
+dir = "firmware/"
+shutil.rmtree(dir, True)
+os.makedirs(dir)
+dir += str(projenv["UNIX_TIME"])
+dir += ".bin"
+shutil.copyfile(".pio/build/nodemcuv2/firmware.bin", dir)
+print(dir)
