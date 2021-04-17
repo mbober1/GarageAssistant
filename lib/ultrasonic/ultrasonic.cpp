124224d0936d826e6d47c85d1e6987c0702ae286
Marcin Bober <mbober1@gmail.com>
2021-04-17 15:31:10 +0200

    leds + sensor

---
+++ lib/ultrasonic/ultrasonic.cpp
@@ -0,0 +1,56 @@
+#include "ultrasonic.hpp"
+
+extern QueueHandle_t distanceQueue;
+unsigned long sensor1Time;
+
+
+static void IRAM_ATTR triggerInterrupt(void* arg)
+{
+    if(!gpio_get_level(GPIO_NUM_2)) {
+        uint distance = (esp_timer_get_time() - sensor1Time)/58;
+        xQueueSendToBack(distanceQueue, &distance, 0);
+    }
+    else sensor1Time = esp_timer_get_time();
+}
+
+
+Ultrasonic::Ultrasonic(gpio_num_t triggerPin, gpio_num_t echoPin, ledc_channel_t pwmChannel) : triggerPin(triggerPin), echoPin(echoPin)
+{
+    const int echo_num = (int)echoPin;
+    int err = 0;
+
+    err += gpio_set_direction(this->triggerPin, GPIO_MODE_OUTPUT);
+    err += gpio_set_level(this->triggerPin, 0);
+
+    err += gpio_install_isr_service(0);
+    gpio_config_t io_conf = {};
+    io_conf.intr_type = GPIO_INTR_ANYEDGE;
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    io_conf.pin_bit_mask = (1ULL<<echo_num);
+    err += gpio_config(&io_conf);
+
+    err += gpio_isr_handler_add(this->echoPin, triggerInterrupt, (void*) &echo_num);
+
+    ledc_timer.duty_resolution = LEDC_TIMER_15_BIT;
+    ledc_timer.freq_hz = 5;
+    ledc_timer.speed_mode = LEDC_HIGH_SPEED_MODE;
+    ledc_timer.timer_num = LEDC_TIMER_0;
+    // ledc_timer.clk_cfg = LEDC_AUTO_CLK;
+    err += ledc_timer_config(&ledc_timer);
+
+    ledc_channel.channel = pwmChannel;
+    ledc_channel.duty = 2;
+    ledc_channel.gpio_num = this->triggerPin;
+    ledc_channel.speed_mode = LEDC_HIGH_SPEED_MODE;
+    ledc_channel.hpoint     = 0;
+    ledc_channel.timer_sel  = LEDC_TIMER_0;
+    
+    err += ledc_channel_config(&ledc_channel);
+
+    if(err) ESP_LOGE("Ultrasonic", "sensor failed");
+    else ESP_LOGE("Ultrasonic", "sensor initialized");
+}
+
+Ultrasonic::~Ultrasonic() {}
--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,46 @@
+#define INCLUDE_vTaskSuspend 1
+
 #include <Arduino.h>
 #include <SmartLeds.h>
-// #include "secrets.hpp"
-//#include <wifi.hh>
-//#include <mqtt.hh>
-// #include "esp32sr04.h"
-// #include "LedRGB.h"
+#include <ultrasonic.hpp>
+#include "secrets.hpp"
+#include <wifi.hh>
+#include <mqtt.hh>
 
-//myWifi wifi(ssid, password);
-//myMQTT mqtt(project_name, mqtt_server, mqtt_port, mqtt_user, mqtt_pass, mqtt_dir, entity_name);
+myWifi wifi(ssid, password);
+myMQTT mqtt(project_name, mqtt_server, mqtt_port, mqtt_user, mqtt_pass, mqtt_dir, entity_name);
 
- unsigned long lastMillis = 0;
- bool stan = 0;
- int distance = 150;
-  
-SmartLed test1(LED_WS2812, 30, 2, 1);
+//ultrasonic sensor config
+const gpio_num_t TRIG = GPIO_NUM_4;
+const gpio_num_t ECHO = GPIO_NUM_2;
+const ledc_channel_t SENSOR_PWM = LEDC_CHANNEL_1;
+
+//leds config
+const uint maxDistance = 20;
+const uint ledCount = 8;
+
+QueueHandle_t distanceQueue;
+
+
+int stan = 0;
+
 
 static void ledTask(void*) {
-  Serial.println("Zaczalem taska");
+  SmartLed leds(LED_WS2812B, ledCount, 12, 0);
+  uint distance;
 
-  //test1[0] = Rgb{0,0,150};
-  //test1.show();
-  Serial.println("Dane poszly");
-    Serial.println("Dupa");
-    
-    if(distance > 200)
-    {
-      Serial.println("Blad");
-    }
-    else if(distance < 2)
-    {
-      for(int i=0; i<30; i++)
-      {
-         test1[i] = Rgb{250,0,0};
-         test1.show();
-      }
-    }
-    else
-    {
-      int k = (int) distance*1.25;
-      for(int i=0; i<30; i++)
-      {
-         test1[i] = Rgb{255-k, k, 0};
-         test1.show();
-      }
-    }
-    
-    test1.show();
+  while(1) {
+    if(xQueueReceive(distanceQueue, &distance, portMAX_DELAY)) {
+      uint percentage = (distance*100)/maxDistance;
+      uint activeLeds = (percentage * ledCount)/100;
+      printf("Distance: %d -> %d%% = %d/%d leds\n", distance, percentage, activeLeds, ledCount);
 
-    delay(1000);
-  
- Serial.println("End task");
+      for(uint i = 0; i < ledCount; ++i) {
+        leds[i] = Rgb{(activeLeds<=i)?(uint8_t)0:(uint8_t)255,0,0};
+      }
+      leds.show();
+    }
+  }
   vTaskDelete(NULL);
 }
 
@@ -57,14 +48,13 @@ static void ledTask(void*) {
 
 void setup() {
   Serial.begin(115200);
- // delay(1000);
-  //Serial.printf("Version: %d\n", LAST_BUILD_TIME);
-  // wifi.connect();
+  Serial.printf("Version: %d\n", LAST_BUILD_TIME);
+  wifi.connect();
   // mqtt.connect(wifi.client);
   // mqtt.configure();
- xTaskCreate(ledTask, "Ledy_Task", 4096, nullptr, 5, NULL); 
-  // initSensors();
-  //init_RGB();
+  distanceQueue = xQueueCreate(5, sizeof(uint));
+  xTaskCreate(ledTask, "Ledy_Task", 4096, nullptr, 5, NULL); 
+  Ultrasonic sensor(TRIG, ECHO, SENSOR_PWM);
 }
 
 
@@ -72,19 +62,8 @@ void setup() {
 
 void loop() {
   // mqtt.loop();
-  
-
-  Serial.println("xD");
+  // mqtt.autoStatus(stan);
+  stan = !stan;
+  printf("MQTT auto status %d\n", stan);
   delay(1000);
-  // show_RGB(20);
-  // int czujnik1 = espSr04.distance1_cm;
-  // int czujnik2 = espSr04.distance2_cm;
-  
- /* test1.show();
-  if (millis() - lastMillis > 1000) {
-    // mqtt.autoStatus(stan);
-    Serial.println(stan);
-    stan = !stan;
-    lastMillis = millis();
-  }*/
 }
